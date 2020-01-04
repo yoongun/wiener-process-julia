c3c1399842d16f746bb8d048c93e46bedce12168
cfr2ak <cfr2ak@gmail.com>
2020-01-04 15:52:03 +0900

    Change plot using seriestype instead

--- WeinerProcess.jl
+++ WeinerProcess.jl
@@ -8,6 +8,6 @@ trace = [[rand(50)]; [rand(50)]; [rand(50)]]
 
 # trace = [[1 2 3]; [1 2 3]; [1 2 3]]
 
-display(scatter(trace[1, :], trace[2, :], trace[3, :], markersize=2))
+display(plot(trace[1, :], trace[2, :], trace[3, :], seriestype=:scatter, markersize=2))
 readline()
 
