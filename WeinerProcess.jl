dbd8cfb3127d781ffb6da58c9748e3f8434ff47d
cfr2ak <cfr2ak@gmail.com>
2020-01-04 15:41:10 +0900

    :innocent: Add very basic random scatter plot

--- WeinerProcess.jl
+++ WeinerProcess.jl
@@ -1,5 +1,13 @@
+using Plots
+using Random
 
-space = (Int8, (512, 512, 512))
 
-initialPosition = 
+Random.seed!(2020)
+n = 50
+trace = [[rand(50)]; [rand(50)]; [rand(50)]]
+
+# trace = [[1 2 3]; [1 2 3]; [1 2 3]]
+
+display(scatter(trace[1, :], trace[2, :], trace[3, :], markersize=2))
+readline()
 
