2caa221c97ca008a83e6d548e1dbf9085ec54d93
cfr2ak <cfr2ak@gmail.com>
2020-01-04 16:41:41 +0900

    :tada: Finish!
    With result of animation

--- WeinerProcess.jl
+++ WeinerProcess.jl
@@ -4,15 +4,23 @@ using Random, Distributions
 Random.seed!(2020)
 
 d = Normal()
-trace = [Vector{Float64}; Vector{Float64}, Vector{Float64}]
+trace_x = Vector{Float64}()
+trace_y = Vector{Float64}()
+trace_z = Vector{Float64}()
 
 # Initial position
-append!(trace[1], 0)
-append!(trace[2], 0)
-append!(trace[3], 0)
+append!(trace_x, 0)
+append!(trace_y, 0)
+append!(trace_z, 0)
 
 anim = @animate for i=1:1000
     dx, dy, dz = rand(d, 3)
-    plot(trace[1, :], trace[2, :], trace[3, :], seriestype=:scatter, markersize=2)
+    append!(trace_x, trace_x[i] + dx)
+    append!(trace_y, trace_y[i] + dy)
+    append!(trace_z, trace_z[i] + dz)
+
+    plot(trace_x, trace_y, trace_z, markersize=2)
 end
+
+gif(anim, "./fps15.gif", fps=15)
 
